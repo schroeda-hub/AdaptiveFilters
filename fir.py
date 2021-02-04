a115b168b8adeb44069ce4441f9610a18b29de58
Andreas Schroeder <schroeder@mbe-komponenten.de>
2021-02-04 17:23:44 +0100

    [feature] add test and filter of sequence

--- fir.py
+++ fir.py
@@ -8,11 +8,20 @@ class fir(object):
         Args:
             b (list): numerator of a linear filter
         """
+        self.set_b(b)
+        
+    def set_b(self, b):
         self.b = b
         self.N = len(b)
-        self.a = [0] * self.N
-        self.a[1] = 1
         self.impulsAnswer = self.calc_impz()
+        self.x = np.zeros(self.N)
+        
+    def _update_x(self, x:float):
+        for i in range(self.x.size-1,0,-1):
+            self.x[i] = self.x[i-1]
+            # print("{} -> {}".format(i-1,i))
+            pass
+        self.x[0] = x
     
     def calc_impz(self):
         """Calculate the impulse answer
@@ -25,7 +34,7 @@ class fir(object):
             impz += "+{}z^(-{})".format(self.b[i], i)
         return impz
             
-    def filter(self, signal):
+    def filter_signal(self, signal):
         """Filter a signal using this linear filter
 
         Args:
@@ -38,3 +47,29 @@ class fir(object):
         for i in range(self.N, signal.size):
             output[i] = signal[i-self.N:i] @ self.b
         return output
+    
+    def filter(self, signal):
+        self._update_x(signal)
+        return self.x.T @ self.b
+    
+    def angle_to_complex(self, angle):
+        return np.exp(1j*angle*2*np.pi/360)
+    
+    def calc_from_zPlane(self, coordinates:list):
+        ret_val = np.poly(coordinates)
+        return ret_val
+
+def _test():
+    test_fir = fir([])
+    zeros_location = [
+        1, 
+        0+1j, 0-1j,
+        test_fir.angle_to_complex(10), test_fir.angle_to_complex(-10),
+        test_fir.angle_to_complex(20), test_fir.angle_to_complex(-20),
+        test_fir.angle_to_complex(30), test_fir.angle_to_complex(-30)
+    ]
+    test_fir.set_b(test_fir.calc_from_zPlane(zeros_location))
+    pass
+    
+if __name__ == '__main__':
+    _test()
